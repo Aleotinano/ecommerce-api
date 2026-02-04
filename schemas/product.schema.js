0249a4bf38aff7880f4387cdde209b946c31d5d6
Aleotinano <alejandrootinano2@gmail.com>
2026-02-04 15:19:59 -0300

    correciOn en schema de validacion de productId

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -47,3 +47,7 @@ export const productQuery = z.object({
   limit: z.coerce.number().int().positive().max(100).default(10),
   offset: z.coerce.number().int().min(0).default(0),
 });
+
+export const productId = z.object({
+  productId: z.coerce.number().int().positive("ID del producto inválido"),
+});
