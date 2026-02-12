56d562b22b4c866ba236e85817a6971657520b7e
Aleotinano <alejandrootinano2@gmail.com>
2026-02-12 17:37:47 -0300

    - variables de admin

--- config.js
+++ config.js
@@ -2,12 +2,8 @@ export const DEFAULTS = {
   PORT: process.env.PORT,
   ORIGINS: process.env.ORIGINS?.split(","),
   SECRET_JWT_KEY: process.env.SECRET_JWT_KEY,
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV || "production",
   SALT_ROUNDS: 10,
   LIMIT: 10,
   OFFSET: 0,
-  ADMIN: {
-    USERNAME: process.env._USERNAME,
-    PASSWORD: process.env._PASSWORD,
-  },
 };
